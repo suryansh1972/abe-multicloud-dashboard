53cc6104f16f1a7396753b440a438aed1954500e
sanju9922 <sanjaykumar090902@gmail.com>
2025-05-02 19:25:59 +0530

    Implemented noise addition and removal in the encryption algorithm for enhanced security

--- abe/cpabe_utils.py
+++ abe/cpabe_utils.py
@@ -1,90 +1,133 @@
 from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import os
 import base64
-import json
 import pickle
+import re
+import random
+import struct
+
 
 class SimpleABE:
     def __init__(self):
         self.key_store = {}
         self.policy_store = {}
         os.makedirs('abe/keys', exist_ok=True)
-        
+        os.makedirs('decrypted', exist_ok=True)
+        self._load_metadata()
+
     def setup(self):
         """Initialize the encryption system"""
         return True
-        
-    def encrypt_file(self, file_path, attributes):
-        """Encrypt a file with the given attributes"""
-        # Generate a new encryption key
+    
+    def _add_noise(self, data, noise_blocks=3):
+        """Insert noise blocks randomly into the data"""
+        original_len = len(data)
+        positions = sorted(random.sample(range(noise_blocks + 1), noise_blocks))
+        blocks = []
+        cursor = 0
+
+        for i in range(noise_blocks + 1):
+            end = cursor + original_len // (noise_blocks + 1)
+            real_block = data[cursor:end]
+            cursor = end
+
+            noise = os.urandom(len(real_block))  # random noise block
+            if i in positions:
+                blocks.append(noise + real_block)
+            else:
+                blocks.append(real_block + noise)
+
+        noisy_data = b''.join(blocks)
+
+        # Pack header: total_blocks + indices of real blocks
+        header = struct.pack("B" + "B" * len(positions), len(blocks), *positions)
+        return header + noisy_data
+
+    def _remove_noise(self, noisy_data):
+        """Remove noise blocks using header"""
+        total_blocks = noisy_data[0]
+        positions = list(noisy_data[1:1 + total_blocks - 1])
+        body = noisy_data[1 + total_blocks - 1:]
+        block_len = len(body) // total_blocks
+        clean_data = b''
+
+        for i in range(total_blocks):
+            block = body[i * block_len: (i + 1) * block_len]
+            if i in positions:
+                clean_data += block[len(block) // 2:]
+            else:
+                clean_data += block[:len(block) // 2]
+        return clean_data
+
+    def encrypt_file(self, file_path, policy):
+        """Encrypt a file with a Boolean attribute-based policy string"""
         key = Fernet.generate_key()
         cipher_suite = Fernet(key)
-        
-        # Read and encrypt the file
+
         with open(file_path, 'rb') as f:
             file_data = f.read()
-        
+
         encrypted_data = cipher_suite.encrypt(file_data)
-        
-        # Store the key and policy
+
         file_id = os.path.basename(file_path)
         self.key_store[file_id] = key
-        self.policy_store[file_id] = attributes
-        
-        # Save encrypted file
+        self.policy_store[file_id] = policy
+
         encrypted_file_path = f"{file_path}.enc"
         with open(encrypted_file_path, 'wb') as f:
             f.write(encrypted_data)
-            
-        # Save metadata
+
         self._save_metadata()
-        
         return encrypted_file_path
-        
+
     def decrypt_file(self, encrypted_file_path, user_attributes):
-        """Decrypt a file if the user has the required attributes"""
+        """Decrypt file if user satisfies the Boolean attribute policy"""
         file_id = os.path.basename(encrypted_file_path).replace('.enc', '')
-        
+
         if file_id not in self.key_store:
             raise Exception("File not found in key store")
-            
-        # Check if user has required attributes
-        required_attributes = set(self.policy_store[file_id])
-        user_attributes = set(user_attributes)
-        
-        if not required_attributes.issubset(user_attributes):
+
+        policy = self.policy_store[file_id]
+        if not self._evaluate_policy(policy, user_attributes):
             raise Exception("User does not have required attributes for decryption")
-            
-        # Decrypt the file
+
         key = self.key_store[file_id]
         cipher_suite = Fernet(key)
-        
+
         with open(encrypted_file_path, 'rb') as f:
             encrypted_data = f.read()
-            
+
         decrypted_data = cipher_suite.decrypt(encrypted_data)
-        
-        # Save decrypted file
-        decrypted_file_path = os.path.join('decrypted', os.path.basename(encrypted_file_path).replace('.enc', ''))
+
+        decrypted_file_path = os.path.join('decrypted', file_id)
         with open(decrypted_file_path, 'wb') as f:
             f.write(decrypted_data)
-            
+
         return decrypted_file_path
-        
+
+    def _evaluate_policy(self, policy_str, user_attributes):
+        """Evaluate Boolean attribute policy"""
+        def repl(match):
+            attr = match.group(0)
+            return str(attr in user_attributes)
+
+        expr = re.sub(r'\b\w+\b', repl, policy_str)
+        expr = expr.replace("AND", "and").replace("OR", "or")
+
+        try:
+            return eval(expr)
+        except Exception as e:
+            raise Exception(f"Invalid policy format: {policy_str}") from e
+
     def _save_metadata(self):
-        """Save encryption metadata to disk"""
         metadata = {
             'key_store': {k: base64.b64encode(v).decode() for k, v in self.key_store.items()},
             'policy_store': self.policy_store
         }
-        
         with open('abe/keys/metadata.pickle', 'wb') as f:
             pickle.dump(metadata, f)
-            
+
     def _load_metadata(self):
-        """Load encryption metadata from disk"""
         try:
             with open('abe/keys/metadata.pickle', 'rb') as f:
                 metadata = pickle.load(f)
@@ -93,17 +136,14 @@ class SimpleABE:
         except FileNotFoundError:
             pass
 
-# Create a global instance
+# Global instance
 abe = SimpleABE()
 
 def setup():
-    """Initialize the ABE system"""
     return abe.setup()
 
-def encrypt_file(file_path, attributes):
-    """Encrypt a file with the given attributes"""
-    return abe.encrypt_file(file_path, attributes)
+def encrypt_file(file_path, policy):
+    return abe.encrypt_file(file_path, policy)
 
 def decrypt_file(encrypted_file_path, user_attributes):
-    """Decrypt a file if the user has the required attributes"""
     return abe.decrypt_file(encrypted_file_path, user_attributes)
---
+++ sample.py
@@ -0,0 +1,20 @@
+def funcA(str3,str1,str2):
+    
+
+    str3.append(3)
+    str3.append(4)
+
+def funcB(a,b):
+    c= a+b
+def main(): 
+    print("aaa")
+    str1=[]
+    str2=[]
+    str3= []
+    str1.append(1)
+    
+    str2.append(3)
+    funcA(str3,str1,str2)
+    print("String 3 is",str3)
+
+main()
